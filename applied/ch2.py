45aaf5c567a0c215c5c07d4aa0fa24d746dabe9d
Will <williamperlichek@gmail.com>
2025-08-14 09:03:11 -0700

    python exceptions

--- applied/ch2.py
+++ applied/ch2.py
@@ -6,8 +6,8 @@ COLLEGE_DATA = "college.csv"
 def read_csv(path):
     try:
         return pd.read_csv(path)
-    except:
-        print("Problem reading csv")
+    except Exception as e:
+        print("Problem reading csv {}", e)
 
 print("Reading in CSV...")
 csv_file = read_csv(BASE_PATH + COLLEGE_DATA)
